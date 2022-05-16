bde9827455edf7ac360bd28b07f11ebf9ce231d3
ODOBRAN <Good.Morning.Vietnam.1991>
2022-05-16 12:01:48 +0300

    Initial commit: setup project, added express, serve static html and css

---
+++ app.js
@@ -0,0 +1,21 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const req = require("express/lib/request");
+const path = require("path");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "not-found.html"));
+});
+
+app.listen(3000);
